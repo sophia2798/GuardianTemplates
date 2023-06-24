d061cb919d44df677b9095e989e9fae7eb8c9815
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-24 01:10:16 -0700

    changing from npm to maven to write in java

---
+++ class-templates/src/main/java/guardiantemplates/App.java
@@ -0,0 +1,13 @@
+package guardiantemplates;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
